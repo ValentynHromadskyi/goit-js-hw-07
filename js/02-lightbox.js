3e55f2efa186d2e48cd40fd5c6974a1bf848915a
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2023-10-27 18:37:58 +0300

    js-hw-07 part 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list=document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarcup(galleryItems))
+
+function createMarcup(arr){
+    return arr.map(({preview, original, description})=>
+   `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+     <img
+       class="gallery__image"
+       src=${preview}
+       data-source=${original}
+       alt=${description}
+       title="${description}"
+     />
+   </a>
+ </li>` 
+    ).join("")
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250  });
+
